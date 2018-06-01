f7edc1b0433a753e85a46650f5cecb2958e9c62f
bujjibabu <bujji404@gmail.com>
2018-06-01 20:58:56 +0530

    bootstrap basics

--- Sandeep/Coferencecall/Conference.js
+++ Sandeep/Coferencecall/Conference.js
@@ -1,14 +1,16 @@
 function english(){
-document.getElementById("audioInfo").innerText = "Audio Information";
-document.getElementById('dailIn').innerText=  "Dail-in number:";
-document.getElementById('accessCode').innerText=  "Access code: ";
-document.getElementById('meetingId').innerText=  "Online meeting ID: ";
-document.getElementById('international').innerText=  "International dial-in numbers:";
+	document.getElementById("audioInfo").innerText = "Audio Information";
+	document.getElementById('dailIn').innerText=  "Dail-in number:";
+	document.getElementById('accessCode').innerText=  "Access code: ";
+	document.getElementById('meetingId').innerText=  "Online meeting ID: ";
+	document.getElementById('international').innerText=  "International dial-in numbers:";
 };
 function dutch(){
-document.getElementById("audioInfo").innerText ="Audio-informatie";
-document.getElementById('dailIn').innerText="Inbel nummer:";
-document.getElementById('accessCode').innerText="Toegangscode:";
-document.getElementById('meetingId').innerText="Online vergadering-ID:";
-document.getElementById('international').innerText="Internationale inbel nummers:";
+	document.getElementById("audioInfo").innerText ="Audio-informatie";
+	document.getElementById('dailIn').innerText="Inbel nummer:";
+	document.getElementById('accessCode').innerText="Toegangscode:";
+	document.getElementById('meetingId').innerText="Online vergadering-ID:";
+	document.getElementById('international').innerText="Internationale inbel nummers:";
 };
+
+english();
